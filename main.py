2e6b9b08d46ae1190aa68bc4bb931b51f084a496
Yasmin Cavalcanti <Ynathaly58@gmail.com>
2025-10-27 12:43:56 -0300

    Novo commit main.py

--- main.py
+++ main.py
@@ -43,3 +43,4 @@ def salvar_aluno(nome: str = Form(...), nota: float = Form(...)):
     alunos.append({ "nome": nome, "nota": nota} )
     return RedirectResponse(url="/", status_code=303)
 #Atualizando
+###
